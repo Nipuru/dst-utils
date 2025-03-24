e8b7b65487cb2909930ce3cd30648fcc846e6257
Nipuru <2789129195@qq.com>
2025-03-24 22:04:47 +0800

    feat: 新增模组设置功能

--- modinfo.lua
+++ modinfo.lua
@@ -2,11 +2,12 @@
 name = "飘然工具箱"
 description = "[饥友们一起玩]服务器系列mod"
 author = "nipuru"
-version = "1.4"
+version = "1.5"
 
 forumthread = ""
 
-api_version = 10
+api_version = 6
+api_version_dst = 10
 
 dont_starve_compatible = false
 reign_of_giants_compatible = false
@@ -18,4 +19,121 @@ client_only_mod = false
 priority = -999999
 server_filter_tags = {}
 
-configuration_options ={}
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+configuration_options =
+{
+    {
+        name = "crops_preserve",
+        label = "作物成熟后不腐烂",
+		hover = "开启后作物成熟后不腐烂",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "eyeturret_recover",
+        label = "炮塔可解除部署",
+		hover = "开启后作物炮塔可右键拆除部署",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "houndstooth_blowpipe_infinite",
+        label = "嚎弹炮无限容量",
+		hover = "开启后嚎弹炮无限装填弹药",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "beargerfur_sack_infinite",
+        label = "熊罐无限容量",
+		hover = "开启后熊罐无限堆叠食物",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "beargerfur_sack_preserver",
+        label = "熊罐保鲜",
+		hover = "开启后熊罐永久保鲜",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "seedpouch_preserver",
+        label = "种子袋保鲜",
+		hover = "开启后种子袋永久保鲜",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "cage_preserver",
+        label = "鸟笼保鲜",
+		hover = "开启后鸟笼永久保鲜",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "fridge_preserver",
+        label = "冰箱保鲜",
+		hover = "开启后冰箱冰切永久保鲜",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "saltbox_preserver",
+        label = "盐箱保鲜",
+		hover = "开启后盐箱永久保鲜",
+        options =
+        {
+            {description = "开启", data = true},
+            {description = "关闭", data = false},
+        },
+        default = false,
+    },
+    {
+        name = "cook_rate",
+        label = "烹饪提速",
+		hover = "提升烹饪速度",
+        options =
+        {
+            {description = "默认", data = 1},
+            {description = "二倍速", data = 0.5},
+            {description = "四倍速", data = 0.25},
+            {description = "立即完成", data = 0},
+        },
+        default = 1,
+    }
+}
--- modmain.lua
+++ modmain.lua
@@ -1,7 +1,86 @@
-local _G = GLOBAL
-local TheNet = _G.TheNet
+local TUNING = GLOBAL.TUNING
+local TheNet = GLOBAL.TheNet
 
 local IsServer = TheNet:GetIsServer() or TheNet:IsDedicated()
 if IsServer then
-    modimport("scripts/settings.lua")
+    -- 冰箱冰切 保鲜 
+    if GetModConfigData("fridge_preserver") then
+        TUNING.PERISH_FRIDGE_MULT = 0
+    end
+    
+
+    -- 盐盒保鲜
+    if GetModConfigData("saltbox_preserver") then
+        TUNING.PERISH_SALTBOX_MULT = 0
+    end
+    
+
+    -- 鸟笼保鲜
+    if GetModConfigData("cage_preserver") then
+        TUNING.PERISH_CAGE_MULT = 0
+    end
+
+    -- 熊罐保鲜
+    if GetModConfigData("beargerfur_sack_preserver") then
+        TUNING.BEARGERFUR_SACK_PRESERVER_RATE = 0
+    end
+
+    -- 种子袋保鲜
+    if GetModConfigData("seedpouch_preserver") then
+        TUNING.SEEDPOUCH_PRESERVER_RATE = 0
+    end
+
+    -- 烹饪提速
+    TUNING.BASE_COOK_TIME = TUNING.BASE_COOK_TIME * GetModConfigData("cook_rate")
+
+    -- 作物成熟后不腐烂
+    if GetModConfigData("crops_preserve") then
+        AddComponentPostInit("growable", function(self)
+            local old_StartGrowing = self.StartGrowing
+            function self:StartGrowing(time)
+                if self.stages and self.stages[self.stage] and self.stages[self.stage].name ~= nil and 
+                    self.stages[self.stage].name == "full" then
+                    self:StopGrowing()
+                    return
+                end
+                return old_StartGrowing(self,time)
+            end
+            self.inst:DoTaskInTime(0,function()
+                if self.stages ~= nil and self.stages[self.stage] and self.stages[self.stage].name ~= nil and self.stages[self.stage].name == "full" then
+                    self:StopGrowing()
+                end	
+            end)
+        end)
+    end
+
+    --炮塔可解除部署
+    if GetModConfigData("eyeturret_recover") then
+        AddPrefabPostInit("eyeturret", function(inst)
+            local function turnon(inst)
+                inst.on = true
+                inst:Remove()
+                GLOBAL.SpawnPrefab("eyeturret_item").Transform:SetPosition(inst.Transform:GetWorldPosition())
+            end
+            inst:AddComponent("machine")
+            inst.components.machine.turnonfn = turnon
+        end)
+    end
+
+    -- 嚎弹炮无限容量
+    if GetModConfigData("houndstooth_blowpipe_infinite") then
+        AddPrefabPostInit("houndstooth_blowpipe", function(inst)
+            if inst.components.container then
+                inst.components.container:EnableInfiniteStackSize(true)
+            end
+        end)
+    end
+
+    -- 熊罐无限容量
+    if GetModConfigData("beargerfur_sack_infinite") then
+        AddPrefabPostInit("beargerfur_sack", function(inst)
+            if inst.components.container then
+                inst.components.container:EnableInfiniteStackSize(true)
+            end
+        end)
+    end
 end
